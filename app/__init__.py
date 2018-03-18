935b894cceea0a2fe86812ff84e9f14f5ddcf763
Doneil Scotland <doneilscotland@yahoo.com>
2018-03-18 22:38:59 +0000

    Completed project1 final

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "this is a super secure key"  # you should make this more random and unique
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://project1:password123@localhost/project1"
+#app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://project1:password123@localhost/project1"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://pgezgjwaizhwyt:a63c6da35a37acf1143984c4e9aff02c0da00259c9de7756193d01d5e6048c39@ec2-54-204-44-140.compute-1.amazonaws.com:5432/dd7s809ja5aln8"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True  # added just to suppress a warning
 
 db = SQLAlchemy(app)
